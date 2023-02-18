27eda72bde45c14a52373ebc993f126e2b8ced3a
zhangxuerong <lucky30527619ya>
2023-02-18 12:52:48 +0800

    add: 获取每个一级分类下的商品列表信息

--- foodie-dev-api/src/main/java/com/imooc/controller/IndexController.java
+++ foodie-dev-api/src/main/java/com/imooc/controller/IndexController.java
@@ -4,6 +4,7 @@ import com.imooc.enums.YseOrNo;
 import com.imooc.pojo.Carousel;
 import com.imooc.pojo.Category;
 import com.imooc.pojo.vo.CategoryVO;
+import com.imooc.pojo.vo.NewItemsVO;
 import com.imooc.service.CarouselService;
 import com.imooc.service.CategoryService;
 import com.imooc.utils.IMOOCJSONResult;
@@ -60,6 +61,18 @@ public class IndexController {
             return IMOOCJSONResult.errorMsg("参数不能为空");
         }
         List<CategoryVO> list = categoryService.getSubCatList(rootCatId);
+        return IMOOCJSONResult.ok(list);
+    }
+
+    @ApiOperation(value = "获取每个一级分类下的商品列表信息", notes = "获取每个一级分类下的商品列表信息", httpMethod = "GET")
+    @GetMapping("/sixNewItems/{rootCatId}")
+    public IMOOCJSONResult sixNewItems(
+            @ApiParam(name = "rootCatId", value="一级分类id",required = true)
+            @PathVariable Integer rootCatId) {
+        if(rootCatId == null){
+            return IMOOCJSONResult.errorMsg("参数不能为空");
+        }
+        List<NewItemsVO> list = categoryService.getSixNewItemsLazy(rootCatId);
         return IMOOCJSONResult.ok(list);
     }
 }
--- foodie-dev-service/src/main/java/com/imooc/service/CategoryService.java
+++ foodie-dev-service/src/main/java/com/imooc/service/CategoryService.java
@@ -1,8 +1,8 @@
 package com.imooc.service;
 
-import com.imooc.pojo.Carousel;
 import com.imooc.pojo.Category;
 import com.imooc.pojo.vo.CategoryVO;
+import com.imooc.pojo.vo.NewItemsVO;
 
 import java.util.List;
 
@@ -19,5 +19,12 @@ public interface CategoryService {
      * @return
      */
     public List<CategoryVO> getSubCatList(Integer rootCatId);
+
+    /**
+     * 查询首页每个分类下的6条商品信息数据
+     * @param rootCatId
+     * @return
+     */
+    public List<NewItemsVO> getSixNewItemsLazy(Integer rootCatId);
 
 }
